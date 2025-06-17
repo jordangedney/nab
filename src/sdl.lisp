442f29e9e9156e99c243fac3e95528726415d710
Jordan Gedney <jordangedney@gmail.com>
2025-06-17 11:38:22 -0400

    cleaned up; add sdl-event-polling

--- src/sdl.lisp
+++ src/sdl.lisp
@@ -18,110 +18,23 @@
           ;; here (parse-fn) before you can access them.
           collect (call parse-fn event))))
 
+(def parse-event (event-ptr)
+  (= ((event-type-ind (c:mem-ref event-ptr :uint32))
+      (struct-type (gethash event-type-ind
+                            *sdl--event-type-to-struct-table*
+                            's:sdl-common-event))
+      (slots (c:foreign-slot-names struct-type)))
+     (loop for s in slots
+           collect (cons
+                    s (c:foreign-slot-value event-ptr
+                                            `(:struct ,struct-type) s)))))
+
 (defvar sdl--prev-sdl-events nil)
 (def get-sdl-events ()
   (= ((r (poll-sdl-events)))
      (set sdl--prev-sdl-events
           (-> (cons r sdl--prev-sdl-events) (take 5)))
      r))
-
-(def event-mapping ()
-  '(
-    (s:sdl-event-keyboard-added   s:sdl-keyboard-device-event)
-    (s:sdl-event-keyboard-removed s:sdl-keyboard-device-event)
-    (s:sdl-event-key-down         s:sdl-keyboard-event)
-    (s:sdl-event-key-up           s:sdl-keyboard-event)
-    ))
-
-;; Currently unparsed events:
-;; CommonEvent DisplayEvent WindowEvent KeyboardDeviceEvent TextEditingEvent
-;; TextEditingCandidatesEvent TextInputEvent MouseDeviceEvent MouseMotionEvent
-;; MouseWheelEvent JoyAxisEvent JoyBallEvent JoyHatEvent
-;; JoyButtonEvent JoyDeviceEvent JoyBatteryEvent GamepadAxisEvent
-;; GamepadButtonEvent GamepadDeviceEvent GamepadTouchpadEvent GamepadSensorEvent
-;; AudioDeviceEvent CameraDeviceEvent RenderEvent TouchFingerEvent
-;; PenProximityEvent PenMotionEvent PenTouchEvent PenButtonEvent PenAxisEvent
-;; DropEvent ClipboardEvent SensorEvent QuitEvent UserEvent
-;; 
-;; sdl-event-ptr -> (sdl-event-key-down "A" 0 T nil), or w/e
-;; should return all events, which is a firehose so filter it down elsewhere
-(def parse-event (event-ptr)
-  (= ((event-type (c:mem-ref event-ptr 's:sdl-event-type))) ; sdl-event-key-up, etc
-     (cons event-type
-           (case event-type
-             s:sdl-event-key-down (get-keyboard event-ptr)
-             s:sdl-event-key-up (get-keyboard event-ptr)
-             s:sdl-event-mouse-button-down (get-mouse-button event-ptr)
-             s:sdl-event-mouse-button-up (get-mouse-button event-ptr)
-             t nil))))
-
-(def parse-event (event-ptr)
-  (= ((event-type (c:mem-ref event-ptr 's:sdl-event-type))) ; sdl-event-key-up, etc
-     (get-event-type-data
-      event-ptr
-      event-type
-      (c:foreign-slot-names
-       (get-hash event-type *sdl--event-type-to-struct-table*)))))
-
-(mac get-event-type-data (ptr event-type accessors)
-  `(c:with-foreign-slots
-       (,accessors (c:mem-aref ,ptr ',event-type) (:struct ,event-type))
-     (list ,@accessors)))
-
-(def parse-event (event-ptr)
-  (= ((event-type (c:mem-ref event-ptr 's:sdl-event-type)) ; sdl-event-key-up, etc
-      (event-ind (c:mem-ref event-ptr :uint32))
-      (et (gethash event-ind *sdl--event-type-to-struct-table*
-                's:sdl-common-event))
-      (slots (c:foreign-slot-names
-              (gethash event-ind *sdl--event-type-to-struct-table*
-                       's:sdl-common-event))))
-     (eval 
-     `(c:with-foreign-slots
-          (,slots
-           (c:mem-aref ,event-ptr ',et) (:struct ,et))
-        (list ,@slots))))
-  )
-
-(def parse-event (event-ptr)
-  (= ((event-type (c:mem-ref event-ptr 's:sdl-event-type)) ; sdl-event-key-up, etc
-      (event-ind (c:mem-ref event-ptr :uint32))
-      (et (gethash event-ind *sdl--event-type-to-struct-table*
-                's:sdl-common-event))
-      (slots (c:foreign-slot-names
-              (gethash event-ind *sdl--event-type-to-struct-table*
-                       's:sdl-common-event))))
-     (loop for slot in slots
-           collect (cons slot
-                         (c:foreign-slot-value event-ptr
-                                               `(:struct ,et) slot)))))
-
-(main)
-(set *nab--running* nil)
-
-(set bar (poll-sdl-events))
-(pr bar)
-
-(print bar)
-(defun parse-event (event-ptr)
-  (let* ((event-type (c:mem-ref event-ptr 's:sdl-event-type))
-         (struct-type (gethash event-type *sdl--event-type-to-struct-table*
-                               's:sdl-common-event)))
-    (loop for slot in (c:foreign-slot-names struct-type)
-          collect (cons slot (c:foreign-slot-value event-ptr `(:struct ,struct-type) slot)))))
-
-
-(def get-mouse-button (event-ptr) ; MouseButtonEvent
-  (get-event-type-data
-   event-ptr s:sdl-mouse-button-event
-   (s:button s:down s:clicks s:padding s:x s:y)))
-
-(def get-keyboard (event-ptr) ; KeyboardEvent
-  (c:with-foreign-slots ((s:key s::mod s:down s:repeat)
-                         (c:mem-aref event-ptr 's:sdl-keyboard-event)
-                         (:struct s:sdl-keyboard-event))
-    (list (s:sdl-get-key-name s:key) s::mod s:down s:repeat)))
-
 
 ;; Note: GPT generated. Don't trust!
 ;; This mapping must exist as the info isn't in the header code, just the comment
